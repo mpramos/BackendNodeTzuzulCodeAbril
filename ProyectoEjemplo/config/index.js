c7cfac8ed5d1945df883487d22fa1a9d3829dfe5
mpramos <mpramos78915400@gmail.com>
2022-04-26 16:39:53 -0400

    variables de entorno , se adjuntaron librerias como dotenv y cross

---
+++ ProyectoEjemplo/config/index.js
@@ -0,0 +1,6 @@
+require("dotenv").config()
+const config={
+    env: process.env.NODE_ENV,
+    port: process.env.PORT
+}
+module.exports=config
---
+++ ProyectoEjemplo/index.js
@@ -0,0 +1,5 @@
+// PARTE INICIAL DEL PROYECTO
+const { env, port } = require("./config");
+
+console.log(env)
+console.log(port)
